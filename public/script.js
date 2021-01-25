d3e1f540d38b7e42f2b1f71bc3f54fad5665d909
Symon Cristhian <contatosymon@gmail.com>
2021-01-24 23:53:44 -0400

    Delete photos on create and edit pages configuration

--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const { get } = require("browser-sync")
-
 const Mask = {
     apply(input, func){
         setTimeout(function(){
@@ -17,57 +15,109 @@ const Mask = {
     }
 }
 
-const PhotosUpload  = {
+const PhotosUpload = {
+    input: "",
     preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event) {
-        const { files: fileList } = event.target;
-        
+        const { files: fileList } = event.target
+        PhotosUpload.input = event.target
+       
         if (PhotosUpload.hasLimit(event)) return
-
+    
         Array.from(fileList).forEach(file => {
+           
+            PhotosUpload.files.push(file)
+           
             const reader = new FileReader()
-
+    
             reader.onload = () => {
-                const image = new Image();
-                image.src = String(reader.result);
-
-                const div = PhotosUpload.getContainer(image);
-
-                PhotosUpload.preview.appendChild(div);
+                const image = new Image()
+                image.src = String(reader.result)
+    
+                const div = PhotosUpload.getContainer(image)
+    
+                PhotosUpload.preview.appendChild(div)
             }
+    
+            reader.readAsDataURL(file)
+        })
 
-            reader.readAsDataURL(file);
-        });
+        PhotosUpload.input.files = PhotosUpload.getAllFiles()
     },
     hasLimit(event) {
-        const { uploadLimit } = PhotosUpload;
-        const { files: fileList } = event.target;
+        const { uploadLimit, input, preview } = PhotosUpload
+        const { files: fileList } = input
         
         if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`);
-            event.preventDefault();
-            return true;
+            alert(`Send at maximum ${uploadLimit} photos`)
+            event.preventDefault()
+            return true
         }
-        return false;
+
+        const photosDiv = []
+        preview.childNodes.forEach(item => {
+            if(item.classList && item.classList.value == "photo")
+                photosDiv.push(item)
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+
+        if(totalPhotos > uploadLimit){
+            alert(`You reached the maximum limit of photos`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
+    getAllFiles(){
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+        return dataTransfer.files
+
     },
     getContainer(image) {
-        const div = document.createElement('div');
-        div.classList.add('photo');
-
-        div.onclick = () => alert('Remover foto');
-
-        div.appendChild(image);
-
-        div.appendChild(PhotosUpload.getRemoveButton());
-
-        return div;
+        const div = document.createElement('div')
+        div.classList.add('photo')
+    
+        div.onclick = PhotosUpload.removePhoto
+    
+        div.appendChild(image)
+    
+        div.appendChild(PhotosUpload.getRemoveButton())
+    
+        return div
     },
     getRemoveButton() {
-        const button = document.createElement('i');
-        button.classList.add('material-icons');
-        button.innerHTML = "close";
-        return button;
+        const button = document.createElement('i')
+        button.classList.add('material-icons')
+        button.innerHTML = "delete_outline"
+        
+        return button
+    },
+    removePhoto(event) {
+       const photoDiv = event.target.parentNode
+       const photosArray = Array.from(PhotosUpload.preview.children)
+       const index = photosArray.indexOf(photoDiv)
+
+       PhotosUpload.files.splice(index, 1)
+       PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+       photoDiv.remove()
+    },
+    removeOldPhoto(event){
+        const photoDiv = event.target.parentNode
+
+        if(photoDiv){
+            const removedFiles = document.querySelector('input[name="removed_files"]')
+            if(removedFiles){
+                removedFiles.value += `${photoDiv.id},`
+            }
+        }
+        photoDiv.remove()
     }
 }
-
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
+
 const { price, formatPrice } = require('../../lib/utils')
 
 module.exports = {
@@ -19,18 +21,30 @@ module.exports = {
         
     },
     async post(req, res){
+        const keys = Object.keys(req.body)
 
-       let results = await Product.create(req.body)
-       const product = results.rows[0]
-       console.log(product)
-       results = await Category.all()
-       const categories = results.rows
+        for(key of keys){
+            if(req.body[key] == ""){
+                return res.send("Please, fill all fields")
+            }
+        }
 
-       return res.redirect(`products/${product.id}/edit`)
+        if(req.files.length == 0){
+            return res.send("Please, send at least one image")
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
+
+       return res.redirect(`products/${productId}/edit`)
     },
     async edit(req, res){
         let results = await Product.find(req.params.id)
         const product = results.rows[0]
+
         if (!product) return res.send("Product not Found!")
         
         product.old_price = formatPrice(product.old_price)
@@ -39,11 +53,42 @@ module.exports = {
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/edit", { product, categories })
+        results = await Product.files(product.id)
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
+
+        return res.render("products/edit", { product, categories, files })
  
     },
     async put(req, res){
-        
+        const keys = Object.keys(req.body)
+
+        for(key of keys){
+            if(req.body[key] == "" && key != "removed_files"){
+                return res.send("Please, fill all fields")
+            }
+        }
+
+        if(req.files.length != 0){
+            const newFilesPromise = req.files.map( file => 
+                File.create({...file, product_id: req.body.id}))
+
+            await Promise.all(newFilesPromise)
+        }
+
+        if(req.body.removed_files){
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
         req.body.price = price(req.body.price)
         if(req.body.old_price != req.body.price) {
             const oldProduct = await Product.find(req.body.id)
